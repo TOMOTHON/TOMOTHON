8f37b0d34f57811c535138e68d03ca6f11c81476
TOMOTHON <liut4593@wrdsb.ca>
2023-01-07 20:30:06 -0500

    fade in js (I don't get it but it works)

--- js/custom.js
+++ js/custom.js
@@ -83,7 +83,54 @@ jQuery(document).ready(function($){
             "top": '-75'
         });
     }
+// FADE IN ELEMENTS
+$.fn.visible = function(partial) {
+    
+	var $t            = $(this),
+		$w            = $(window),
+		viewTop       = $w.scrollTop(),
+		viewBottom    = viewTop + $w.height(),
+		_top          = $t.offset().top,
+		_bottom       = _top + $t.height(),
+		compareTop    = partial === true ? _bottom : _top,
+		compareBottom = partial === true ? _top : _bottom;
+  
+  return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
 
+};
+
+$(window).scroll(function(event) {
+  
+	$(".module").each(function(i, el) {
+	  var el = $(el);
+	  if (el.visible(true)) {
+		el.addClass("come-in"); 
+	  } 
+	});
+	
+  });
+
+  var win = $(window);
+var allMods = $(".module");
+
+// Already visible modules
+allMods.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible"); 
+  } 
+});
+
+win.scroll(function(event) {
+  
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in"); 
+    } 
+  });
+  
+});
 
 // HIDE MOBILE MENU AFTER CLIKING ON A LINK
 
